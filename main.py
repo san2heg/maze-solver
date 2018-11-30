cdf94ab528ad586ddc4bee5e6b0adce502226bf8
Sanketh Hegde <san2heg@gmail.com>
2018-11-30 03:22:59 -0800

    Backtracking DFS

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import collections
+import time
 
 BASE_URL = 'http://ec2-34-216-8-43.us-west-2.compute.amazonaws.com'
 
@@ -19,9 +21,74 @@ def update_maze(token, action):
     r = requests.post(BASE_URL + '/game', params=params, data=payload)
     return r.json()['result']
 
+def dfs_backtrack(token, visited, prev_action):
+    curr_state = get_maze_state(token)
+    curr_loc = curr_state['current_location']
+    if curr_loc == None:
+        return True
+
+    print("Current Location ==> " + str(curr_loc))
+    visited.add((curr_loc[0], curr_loc[1]))
+
+    directions = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+    num_dirs = {
+        'UP': (0, -1),
+        'DOWN': (0, 1),
+        'LEFT': (-1, 0),
+        'RIGHT': (1, 0)
+    }
+
+    for dir in directions:
+        new_loc = [curr_loc[0] + num_dirs[dir][0], curr_loc[1] + num_dirs[dir][1]]
+        # print('TRYING TO MOVE ' + dir + ' TO ' + str(new_loc))
+        if (new_loc[0], new_loc[1]) in visited:
+            # print('HAS BEEN VISITED')
+            continue
+        resp = update_maze(token, dir)
+        if resp == 'END':
+            print('END FOUND')
+            return True
+        elif resp == 'SUCCESS':
+            print('MOVING ' + dir)
+            result = dfs_backtrack(token, visited, dir)
+            if result:
+                return True
+
+    opposite_dir = {
+        'UP': 'DOWN',
+        'DOWN': 'UP',
+        'LEFT': 'RIGHT',
+        'RIGHT': 'LEFT'
+    }
+
+    update_maze(token, opposite_dir[prev_action])
+    # bt_loc = get_maze_state(token)['current_location']
+    print("BACKTRACKING")
+
+    return False
+
+def solve_maze(token):
+    visited = set()
+    dfs_backtrack(token, visited, None)
+
 def main():
     token = get_token()
-    print(get_maze_state(token))
+    print("Token ==> " + str(token))
+    state = get_maze_state(token)
+
+    start_time = time.time()
+    total_time = 0
+    maze_no = 1
+    while state['status'] != 'FINISHED':
+        state = get_maze_state(token)
+        solve_maze(token)
+        elapsed = time.time() - start_time
+        print("=== MAZE " + str(maze_no) + " SOLVED IN " + str(elapsed) + " ===")
+        total_time += elapsed
+        start_time = time.time()
+        maze_no += 1
+
+    print("Total time taken: " + str(time.time() - orig_start))
 
 if __name__ == "__main__":
     main()
